493c9528e614b3bd3d92417e11f78dd09e50c484
GermanGab <143663789+GermanGab@users.noreply.github.com>
2024-06-13 08:20:36 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .nav');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
